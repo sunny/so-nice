888e0c7029052e795fa7d2cbf8821c9b5abea2f5
Sunny Ripert <negatif@gmail.com>
2009-01-25 14:53:12 +0100

    Added a Player for Rhythmbox!

---
+++ rhythmbox.rb
@@ -0,0 +1,28 @@
+class RhythmboxPlayer
+  def playpause
+    tell_to 'play-pause'
+  end
+  def prev
+    tell_to 'previous'
+  end
+  def next
+    tell_to 'next'
+  end
+  def voldown
+    tell_to 'volume-down'
+  end
+  def volup
+    tell_to 'volume-up'
+  end
+  def volume
+  	tell_to 'print-volume'
+  end
+  def current_track
+    tell_to 'print-playing'
+  end
+
+  private
+  def tell_to(command)
+    %x(rhythmbox-client --#{command}).rstrip
+  end
+end
