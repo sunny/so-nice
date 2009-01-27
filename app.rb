e8aea18ed3863bcb40cdb813e8f54a5841b1f712
Sunny Ripert <negatif@gmail.com>
2009-01-27 16:58:45 +0100

    Don't need sass for such a small css

--- app.rb
+++ app.rb
@@ -28,11 +28,6 @@ get '/' do
   haml :index
 end
 
-get '/stylesheet.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass :stylesheet
-end
-
 use_in_file_templates!
 
 __END__
@@ -61,28 +56,6 @@ __END__
         %input{:type=>'submit', :value => '▸', :name=>'playpause', :title => "Play/Pause"}
         %input{:type=>'submit', :value => '←', :name=>'prev',      :title => "Previous"}
         %input{:type=>'submit', :value => '→', :name=>'next',      :title => "Next"}
-        %input{:type=>'submit', :value => '♪', :name=>'voldown',   :title => "Quieter"} 
+        %input{:type=>'submit', :value => '♪', :name=>'voldown',   :title => "Quieter"}
         %input{:type=>'submit', :value => '♫', :name=>'volup',     :title => "Louder"}
-
-@@ stylesheet
-body
-  padding: 0 5em
-  margin: 0
-  background: white
-  color: #333
-  font: 1.5em Helvetica, sans-serif
-input
-  font-size: 1em
-  color: inherit
-  text-decoration: none
-  padding: .2em .3em
-  background: #2b79d4
-  color: #fff
-  -moz-border-radius: .4em
-  -webkit-border-radius: .4em
-  border: .1em solid #2b79d4
-  cursor: pointer
-  &:hover
-    background: #83aedf
-    border-color: #77a1d1
 
