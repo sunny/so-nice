b32e6df59bd63da25b45d08f52626404b7358ca0
Mark T. Tomczak <iam@fixermark.com>
2010-02-09 17:20:46 -0500

    Implemented support of MusicPlayer class interface

--- players/itunes_win.rb
+++ players/itunes_win.rb
@@ -1,30 +1,34 @@
 class ItunesPlayer_Win < MusicPlayer
   def playpause
-
+    @itunes.PlayPause()
   end
 
   def prev
-
+    @itunes.PreviousTrack()
   end
 
   def next
-
+    @itunes.NextTrack()
   end
 
   def voldown
-
+    @itunes.SoundVolume=@itunes.SoundVolume - 10
   end
 
   def volup
-
+    @itunes.SoundVolume=@itunes.SoundVolume + 10
   end
 
   def volume
-
+    @itunes.SoundVolume
   end
 
   def current_track
-
+    curTrack=@itunes.CurrentTrack
+    if(curTrack)
+      return (curTrack.Artist + " - " + curTrack.name)
+    end
+    ""
   end
 
   def launched?
